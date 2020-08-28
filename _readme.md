4a1d442abe09f278644bf18c4aed2602808bf451
kojihyung <kokoleerh@gmail.com>
2020-08-28 15:59:43 +0900

    새로고침 실패 & 디자인 변경

--- _readme.md
+++ _readme.md
@@ -19,3 +19,6 @@ npm install moment
 npm install --save @fullcalendar/core @fullcalendar/daygrid
 
 healthmng.site 로 도메인 구매해둠 승인 대기중
+
+npm install ws --save // 안씀
+npm install request --save
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
 const router = require('./routes/router');
 var bodyParser = require('body-parser');
+var io = require('socket.io')(server);
+const basicController = require('./controllers/basic');
+const request = require('request');
 
-const {sequelize,DB_INFO} = require("./util/database");
+const { sequelize, DB_INFO } = require("./util/database");
 const session = require("express-session");
 //const Member = require('./models/Member');
 //const BodyInfo = require('./models/bodyInfo');
@@ -13,20 +17,21 @@ const MySQLStore = require("express-mysql-session")(session);
 const store = new MySQLStore(DB_INFO);
 
 const path = require('path');
+const { render } = require('ejs');
 
 app.use(
-    session({
-      secret: "dplandplan",
-      resave: false,
-      saveUninitialized: false,
-      store: store
-    })
-  );
+  session({
+    secret: "dplandplan",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
-  app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-  app.use('/public', express.static('public'));
-  app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
+app.use('/public', express.static('public'));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
 // dirname 뒤가 views와 관련된 디렉토리 주소
 // 예를 들어 dirname + '/views/post' 하면 view와 관련된게 다 post 위치로 잡히더라
@@ -35,14 +40,21 @@ app.set('view engine', 'ejs');
 // 익스프레스 미들웨어 중 하나
 // req.body의 header와 body를 읽을 수 있게 도와줌 아무튼 html 통신을 도와주는 중간자
 app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  extended: true
+}));
 
 app.use(router);
 
 sequelize
   .sync()
   .then(() => {
-    app.listen(3000);
+    server.listen(3000, function () {
+      console.log('Socket IO server listening on port 3000');
+    });
+    io.on('connection', function (socket) {
+      socket.on('clicked', function (data) {
+        console.log("reload");
+      });
+    })
   })
   .then(err => console.log(err));
